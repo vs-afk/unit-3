94be04f02217b93b04df26f56a1121276231ac1a
Vince Saenz <vincesaenz89@gmail.com>
2026-03-03 13:57:44 -0600

    challenge 2 done

--- Challenge2/challenge2.js
+++ Challenge2/challenge2.js
@@ -1,4 +1,4 @@
-const transactions = [250, -100, 400, -50, -300, 1200, -20, 75];
+const transactions = [250, -100, 400, -50, -300, 1200, -20, 75, -750];
 
 function calculateBalance(x) {
     let total = 0;
@@ -9,8 +9,11 @@ function calculateBalance(x) {
     return total;
 }
 
+console.log("Account Summary")
+console.log("---------------")
+
 let balance = calculateBalance(transactions);
-console.log(balance)
+console.log("Balance: " + balance)
 
 function countDeposits(x) {
     let total = 0;
@@ -23,7 +26,7 @@ function countDeposits(x) {
 }
 
 let depositCount = countDeposits(transactions)
-console.log(depositCount)
+console.log('Deposits: ' + depositCount)
 
 function countWithdrawals(x) {
     let total = 0;
@@ -36,4 +39,19 @@ function countWithdrawals(x) {
 }
 
 let withdrawalCount = countWithdrawals(transactions)
-console.log(withdrawalCount)
+console.log('Withdrawal: ' + withdrawalCount)
+
+function detectLargeWithdrawal(x) {
+    let activity;
+    for(let i = 0; i < x.length; i++) {
+        if (x[i] < -500) {
+            activity = "Suspicious Activity Detected"
+        } else {
+            activity = "No Suspicious Activity"
+        }
+    }
+    return activity;
+}
+
+let securityStatus = detectLargeWithdrawal(transactions)
+console.log('Security Status: ' + securityStatus)
